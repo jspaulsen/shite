4bd6d8daf16fce6ba23f45517f8ecc9a6db4418b
jspaulsen <fatiguely@gmail.com>
2019-04-29 10:08:54 -0400

    Add TODO(#10)

--- src/game/state/state_handler.rs
+++ src/game/state/state_handler.rs
@@ -2,7 +2,7 @@ use super::game_state::GameState;
 
 pub type BoxedGameState = Box<GameState>;
 
-// TODO: Implement StateHandler
+// TODO(#10): Implement StateHandler
 struct StateHandler {
     active: Option<BoxedGameState>,
     states: Vec<BoxedGameState>,
