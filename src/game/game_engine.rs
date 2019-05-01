9d369a3c3a0a0d7606979da109c82350583daac9
jspaulsen <fatiguely@gmail.com>
2019-04-30 20:24:10 -0400

    Implement Builder pattern for GameEngine.

--- src/game/game_engine.rs
+++ src/game/game_engine.rs
@@ -9,41 +9,55 @@ pub struct GameEngine {
     pub window_handler: window_handler::WindowHandler,
 }
 
-// TODO(#6): Introduce builder pattern for GameEngine
+pub struct GameEngineBuilder {
+    title: String,
+    width: u32,
+    height: u32,
+}
+
+
 impl GameEngine {
-    /// Returns an instance of the GameEngine object.
-    ///
-    /// # Arguments
-    /// * title - Window Title
-    /// * width - Window width
-    /// * height - Window height
-    ///
-    /// # Returns
-    /// Result<Self, String>
-    pub fn new(
-        title: &str,
-        width: u32,
-        height: u32,
-    ) -> Result<Self, String> {
-        let sdl_context = sdl2::init()?;
-        let window_handler = window_handler::WindowHandler::new(
-            sdl_context.video()?,
-            title,
-            width,
-            height,
-        )?;
-
-        Ok(Self {
-            sdl_context,
-            window_handler,
-        })
-    }
-
     // TODO(#7): Implement GameEngine->run function
     // Run function runs the actual loop.  This will likely require
     // new() parameters to change as result & incorporate ticks_per_second
     // and (eventually) variable or fixed
     pub fn run(&self) -> Result<(), String> {
         Ok(())
+    }
+}
+
+
+impl GameEngineBuilder {
+    /// Returns an instance of the GameEngineBuilder object.
+    ///
+    /// # Arguments
+    /// * title - Title of window when built
+    /// * width - Width of window when built
+    /// * height - Height of window when built
+    ///
+    /// # Returns
+    /// Result<Self, String>
+    pub fn new(title: String, width: u32, height: u32) -> Self {
+        Self {
+            title,
+            width,
+            height
+        }
+    }
+
+    /// Builds the GameEngine
+    pub fn build(self) -> Result<GameEngine, String> {
+        let sdl_context = sdl2::init()?;
+        let window_handler = window_handler::WindowHandler::new(
+            sdl_context.video()?,
+            &self.title,
+            self.width,
+            self.height,
+        )?;
+
+        Ok(GameEngine {
+            sdl_context,
+            window_handler,
+        })
     }
 }
