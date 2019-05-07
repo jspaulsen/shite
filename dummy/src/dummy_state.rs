2e4b7748f93e9ecea61dcf167fd20d9721ea39b3
jspaulsen <fatiguely@gmail.com>
2019-05-07 17:55:22 -0400

    Refactor to simplify base code.  Too many changes to note.

--- dummy/src/dummy_state.rs
+++ dummy/src/dummy_state.rs
@@ -1,39 +1,98 @@
 use sdl2::event::Event;
-use shite::game::{GameState, GameEventHandler, BoxedRenderable};
+use sdl2::mouse::MouseButton;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+
+use shite::engine::Context;
+use shite::graphics::GameTextureCreator;
+use shite::state::{
+    GameState,
+    GameEventHandler,
+};
 
 pub struct DummyState {
-
+    pub x: i32,
+    pub y: i32,
+    pub should_render: bool,
+    pub texture_creator: GameTextureCreator,
 }
 
-// GameEventHandler
+impl DummyState {
+    pub fn new(x: i32, y: i32, should_render: bool, context: &Context) -> Self {
+        Self {
+            x,
+            y,
+            should_render,
+            texture_creator: context.window.get_canvas_ref().texture_creator(),
+        }
+    }
+}
+
 impl GameEventHandler for DummyState {
-    fn on_key_down(&mut self, event: &Event) {
+    fn on_key_down(&mut self, context: &mut Context, event: &Event) {
         println!("on_key_down: {:?}", event);
+        match event {
+            Event::KeyDown { .. } => {
+                println!("on_key_down: {:?}", event);
+            }
+            _ => {}
+        };
     }
 
-    fn on_key_up(&mut self, event: &Event) {
+    fn on_key_up(&mut self, context: &mut Context, event: &Event)  {
         println!("on_key_up: {:?}", event);
     }
 
-    fn on_mouse_motion(&mut self, event: &Event) {
-        println!("on_mouse_motion: {:?}", event);
+    fn on_mouse_motion(&mut self, context: &mut Context, event: &Event)  {
+        match event {
+            Event::MouseMotion { x, y, mousestate, .. } => {
+                self.x = *x;
+                self.y = *y;
+            },
+            _ => {},
+        };
     }
 
-    fn on_mouse_button_down(&mut self, event: &Event) {
-        println!("on_mouse_button_down: {:?}", event);
+    fn on_mouse_button_down(&mut self, context: &mut Context, event: &Event)  {
+        if let Event::MouseButtonDown{ mouse_btn, .. } = event {
+            if let MouseButton::Left = mouse_btn {
+                self.should_render = true;
+            }
+        }
     }
 
-    fn on_mouse_button_up(&mut self, event: &Event) {
-        println!("on_mouse_button_up: {:?}", event);
+    fn on_mouse_button_up(&mut self, context: &mut Context, event: &Event)  {
+        if let Event::MouseButtonUp{ mouse_btn, .. } = event {
+            if let MouseButton::Left = mouse_btn {
+                self.should_render = false;
+            }
+        }
     }
 
-    fn on_mouse_wheel(&mut self, event: &Event) {
+    fn on_mouse_wheel(&mut self, context: &mut Context, event: &Event)  {
         println!("on_mouse_wheel: {:?}", event);
     }
 }
 
 impl GameState for DummyState {
-    fn get_state_name(&self) -> String {
-        return "DummyState".to_string();
+    fn update(&mut self, context: &mut Context) -> Result<(), String> {
+        Ok(())
+    }
+
+    fn draw(&mut self, context: &mut Context) -> Result<(), String> {
+        if self.should_render {
+            let curr_color = context.window.get_canvas_ref().draw_color();
+            let rect = Rect::new(self.x - 13, self.y - 13, 25, 25);
+
+            context.window.get_canvas_mut().set_draw_color(
+                Color::RGB(255, 255, 255)
+            );
+            context.window.get_canvas_mut().draw_rect(rect);
+            context.window.get_canvas_mut().fill_rect(rect);
+
+            context.window.get_canvas_mut().set_draw_color(curr_color);
+        }
+
+        Ok(())
     }
 }
--- dummy/src/main.rs
+++ dummy/src/main.rs
@@ -1,32 +1,19 @@
 mod dummy_state;
 
-use std::slice::Iter;
-
-use sdl2::pixels::Color;
-use shite::game::{GameEngine, GameEngineBuilder};
-
+use uuid::Uuid;
+use shite::engine::{GameEngine, GameEngineBuilder};
+use shite::state::{GameState};
 use dummy_state::DummyState;
 
 
 fn main() {
-    let dummy_state: DummyState = DummyState { };
     let mut engine: GameEngine = GameEngineBuilder::new(
         "Poopster",
         800,
         600,
-    ).add_game_state(Box::new(dummy_state))
-    .set_active_game_state("DummyState".to_string())
-    .build()
-    .unwrap();
+    ).build().unwrap();
+    let context = engine.get_context();
+    let mut dummy_state = DummyState::new(0, 0, false, context);
 
-    engine.run();
+    engine.run(&mut dummy_state);
 }
-
-/*
-fn on_key_down(&mut self, event: &Event);
-fn on_key_up(&mut self, event: &Event);
-fn on_mouse_motion(&mut self, event: &Event);
-fn on_mouse_button_down(&mut self, event: &Event);
-fn on_mouse_button_up(&mut self, event: &Event);
-fn on_mouse_wheel(&mut self, event: &Event);
-*/
